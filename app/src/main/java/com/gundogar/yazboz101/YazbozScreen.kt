b8d4f01017bd686b8aaa23492fea7700f76e37dd
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-03-17 15:03:19 +0300

    feat: Initial Yazboz app structure
    This commit introduces the foundational structure for the Yazboz application, including:
    
    -   `YazbozState`: Defines the state management for the application, initially including an `isLoading` flag.
    -   `YazbozAction`: Placeholder for future actions within the app.
    -   `YazbozScreen`:  A basic composable screen function, accepting a `Modifier`.
    -   `YazbozEvent`: Placeholder for future events within the app.
    -   Configured project VCS and gradle settings.

---
+++ app/src/main/java/com/gundogar/yazboz101/YazbozScreen.kt
@@ -0,0 +1,10 @@
+package com.gundogar.yazboz101
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun YazbozScreen(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/com/gundogar/yazboz101/YazbozState.kt
@@ -0,0 +1,5 @@
+package com.gundogar.yazboz101
+
+data class YazbozState(
+    val isLoading: Boolean = false,
+)
