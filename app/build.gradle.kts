9f30a287111f8f8d5a4f8a598c7e93efb3a2c5ba
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-07-26 10:51:55 +0300

    build: Update app version to 1.2 (code 3)
    This commit increments the application version:
    - `versionCode` is updated from 2 to 3.
    - `versionName` is updated from "1.1" to "1.2".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gundogar.yazboz101"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
