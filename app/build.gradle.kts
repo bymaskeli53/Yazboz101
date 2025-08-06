eb3c977ddb509af02962459dd3a851139326eddb
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-08-06 19:44:08 +0300

    build: Increment app version to 1.4 (code 5)
    This commit updates the application's version information:
    - `versionCode` is incremented from 4 to 5.
    - `versionName` is updated from "1.3" to "1.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gundogar.yazboz101"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
